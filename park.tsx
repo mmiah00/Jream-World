8713e9eb07876a8c287ac09c280fd136ddd4b4f5
chloeho7 <chloeho7@gmail.com>
2022-09-16 21:27:54 -0400

    added dojo and park files

---
+++ park.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="park" tilewidth="175" tileheight="190" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="96">
+  <image width="32" height="25" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Box.png"/>
+ </tile>
+ <tile id="97">
+  <image width="15" height="13" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Garbage_Can1.png"/>
+ </tile>
+ <tile id="98">
+  <image width="15" height="13" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Garbage_Can2.png"/>
+ </tile>
+ <tile id="99">
+  <image width="23" height="32" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Ladder1.png"/>
+ </tile>
+ <tile id="100">
+  <image width="23" height="32" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Ladder2.png"/>
+ </tile>
+ <tile id="101">
+  <image width="92" height="48" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Ramp1.png"/>
+ </tile>
+ <tile id="102">
+  <image width="92" height="48" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Ramp2.png"/>
+ </tile>
+ <tile id="103">
+  <image width="144" height="39" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Rapm3.png"/>
+ </tile>
+ <tile id="104">
+  <image width="26" height="7" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Skateboard1.png"/>
+ </tile>
+ <tile id="105">
+  <image width="11" height="20" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Skateboard2.png"/>
+ </tile>
+ <tile id="106">
+  <image width="10" height="20" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Skateboard3.png"/>
+ </tile>
+ <tile id="107">
+  <image width="12" height="20" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Skateboard4.png"/>
+ </tile>
+ <tile id="108">
+  <image width="62" height="103" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Tree1.png"/>
+ </tile>
+ <tile id="109">
+  <image width="124" height="129" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Tree2.png"/>
+ </tile>
+ <tile id="110">
+  <image width="175" height="190" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Tree3.png"/>
+ </tile>
+ <tile id="111">
+  <image width="175" height="190" source="../../../Downloads/Free Park Zone Tileset/3 Objects/Other/Tree4.png"/>
+ </tile>
+</tileset>
