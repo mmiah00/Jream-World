2e48d00d1128d1ceb00187de454243ba73e92b24
gjmaria <mari.gg098@gmail.com>
2022-09-16 20:08:40 -0400

    Added tsx files
    added tsx files to fix dance room

---
+++ WA_Exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="WA_Exterior" tilewidth="32" tileheight="32" tilecount="850" columns="25">
+ <image source="tilesets/WA_Exterior.png" width="800" height="1088"/>
+</tileset>
